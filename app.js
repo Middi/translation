9ff45643534bdf9aec3a5340412acb10662e7812
“Richard Middleton” <Richard@richardmiddleton.me>
2017-07-11 19:05:58 +0200

    Login pages and hidden new entry page

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-var expressSanitizer = require("express-sanitizer");
+const expressSanitizer = require("express-sanitizer");
 const mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var passportLocalMongoose = require("passport-local-mongoose");
 const bodyParser = require('body-parser');
+var User = require("./models/user");
 
 
 var port = process.env.PORT || 3000;
@@ -17,6 +21,31 @@ app.use(expressSanitizer());
 
 // Set Public Folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Table Plate",
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+// =====================
+// Check logged in
+// =====================
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   next();
+});
 
 
 // MONGOOSE/MODEL CONFIG
@@ -38,7 +67,7 @@ app.get('/', function(req, res){
 });
 
 
-app.get('/new', function(req, res){
+app.get('/new', isLoggedIn, function(req, res){
     Trans.find( { category: "Directions" }, function(err, data) {
         res.render('new', {data: data});
     });
@@ -56,6 +85,35 @@ app.post("/entry", function(req, res){
         }
     })
 });
+
+
+// LOGIN ROUTES
+app.get("/login", function(req, res){
+   res.render("login"); 
+});
+
+
+// Login Logic
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/new",
+    failureRedirect: "/login"
+}), function(req, res){
+   
+});
+
+
+// Log Out Logic
+app.get("/logout", function(req,res){
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 // Start Server
 app.listen(port, function () {
