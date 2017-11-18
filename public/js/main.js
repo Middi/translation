36ce4c9dc233f23eed95120d4dea4117f54d504c
“Richard Middleton” <Richard@richardmiddleton.me>
2017-11-18 22:03:12 +0000

    revert

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,4 @@
 $('#myTabs a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
-});
-
-if (window.location.href.indexOf("norwegian") > -1) {
-     $('.flag').attr('src', '/images/norway.png');
-}
+})
